8272d6de52247ff1b83d10e0de2617a69d1150e7
Martin Cespedes <mcespedescubillos@gmail.com>
2023-07-03 19:09:41 -0400

    Added mongoose user model + scrypt hashing pw

--- Tickets/auth/src/index.ts
+++ Tickets/auth/src/index.ts
@@ -5,6 +5,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import mongoose from 'mongoose'
 
 const app = express();
 app.use(json());
@@ -14,6 +15,17 @@ app.use(signoutRouter);
 app.use(signupRouter);
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to mongodb');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 app.listen(3000, () => {
     console.log('Listening on port 3000!')
 })
+
+start();
--- Tickets/auth/src/routes/signup.ts
+++ Tickets/auth/src/routes/signup.ts
@@ -1,29 +1,42 @@
-import express, {Request, Response, NextFunction} from 'express';
-import {body, validationResult} from 'express-validator'
-import CustomValidationError from '../errors/validation-error'
-import DBError from '../errors/db-error'
+import express, { Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
+import CustomValidationError from "../errors/validation-error";
+import DBError from "../errors/db-error";
+import { User } from "../models/user";
+import {BadRequestError} from "../errors/bad-request-error"
 
 const router = express.Router();
 
-router.post('/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid'),
-    body('password')
-        .trim()
-        .isLength({min: 4, max: 20})
-        .withMessage('Password must be between 4 and 20 characters')
-], async (req: Request, res: Response, next: NextFunction) => {
+router.post(
+  "/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return next(new CustomValidationError(errors.array()))
+      return next(new CustomValidationError(errors.array()));
     }
 
     const { email, password } = req.body;
 
-    console.log("creatin a user...")
-    return next(new DBError())
-})
+    const existingUser = await User.findOne({ email });
 
-export { router as signupRouter }
+    if (existingUser) {
+      console.log('Email in use');
+      const err = new BadRequestError("Email in use");
+      return next(err);
+    }
+
+    const user = User.build({email, password})
+    await user.save();
+    res.status(201).send(user);
+  }
+);
+
+export { router as signupRouter };
