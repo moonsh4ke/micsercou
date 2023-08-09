6bbafdcbbc8aa9c35c0f0de114726c0a4f399c55
Martin Cespedes <mcespedescubillos@gmail.com>
2023-08-08 22:01:39 -0400

    app wrapper component

--- Tickets/client/pages/_app.js
+++ Tickets/client/pages/_app.js
@@ -1,5 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client'
+import Header from '../components/Header.jsx'
 
-export default ({Component, pageProps}) => {
-  return <Component />
+export default function AppComponent({Component, pageProps, currentUser}) {
+  return (
+    <>
+      <Header currentUser={currentUser}/>
+      <Component {...pageProps}/>
+    </>
+  )
+}
+
+AppComponent.getInitialProps = async function(app) {
+  const client = buildClient(app.ctx);
+  const {data} = await client.get('api/users/currentuser');
+  let pageProps = {}
+  if(app.Component.getInitialProps) {
+    pageProps = app.Component.getInitialProps(app.ctx);
+  }
+  return {
+    pageProps,
+    ...data
+  }
 }
--- Tickets/client/pages/index.js
+++ Tickets/client/pages/index.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import buildClient from '../api/build-client'
 
-export default function LandingPage({ currentUser }) {
-  return <h1>Landing Page</h1>;
+export default function LandingPage() {
+  return (
+      <h1>Landing Page</h1>
+  );
 }
-
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const {data} = await client.get('/api/users/currentuser')
-  return data
-};
